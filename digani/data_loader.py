aabd015875a8a6e02ac61254b0aa703e33235145
ZwEin <zwein27@gmail.com>
2016-07-07 13:03:37 -0700

    update domain file paths

--- digani/data_loader.py
+++ digani/data_loader.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-07-07 12:36:42
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-07-07 12:45:17
+# @Last Modified time: 2016-07-07 13:03:30
 
 import os
 
@@ -10,15 +10,17 @@ import os
 class DataLoader():
 
     @staticmethod
-    def load_file_paths(root_dir):
-        paths = []
-        for subdir, dirs, files in os.walk(root_dir):
-            for file in files:
-                if '_extractions' in file:
-                    path = os.path.join(subdir, file)
-                    print path
-                    paths.append(path)
-        return paths
+    def load_domain_file_paths(root_dir):
+        df_paths = {}
+        for domain in os.listdir(root_dir):
+            paths[domain] = []
+
+            for subdir, dirs, files in os.walk(os.path.join(root_dir, domain)):
+                for file in files:
+                    if '_extractions' in file:
+                        path = os.path.join(subdir, file)
+                        paths[domain].append(path)
+        return df_paths
 
 if __name__ == '__main__':
     DataLoader().load_file_paths('../../dig-data/sample-datasets/escorts/')
