a3cb0ca66bef19b22b0a41de3e51beddf1b5adc6
ZwEin <zwein27@gmail.com>
2016-07-09 10:36:16 -0700

    trie obj

--- digani/res/city/__init__.py
+++ digani/res/city/__init__.py
@@ -2,21 +2,29 @@
 # @Author: ZwEin
 # @Date:   2016-07-08 13:40:38
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-07-08 14:05:00
+# @Last Modified time: 2016-07-09 10:36:09
 
 import os
 import json
+import pygtrie
 
 RES_CITY_NAMES_PATH = os.path.join(os.path.dirname(__file__), 'names.json')
 
-def load_city_names():
-    with open(RES_CITY_NAMES_PATH, 'rb') as file_handler:
-        city_names = json.load(file_handler)
-        return city_names
+def load_city_names(city_names_path=RES_CITY_NAMES_PATH):
+    names = json.load(codecs.open(self.city_names_path, 'r', 'utf-8'))
+    names_trie_obj = pygtrie.CharTrie()
+    for name in names:
+        names_trie_obj[name] = name
+    return names_trie_obj
 
-res_city_names = None
-if not res_city_names:
-    res_city_names = load_city_names()
+    # with open(city_names_path, 'rb') as file_handler:
+    #     city_names = json.load(file_handler)
+    #     return city_names
+
+city_names_trie_obj = None
+if not city_names_trie_obj:
+    city_names_trie_obj = load_city_names()
 
 if __name__ == '__main__':
-    print res_city_names[:4]
+    # print res_city_names[:4]
+    pass
--- digani/spark_workflow.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: ZwEin
-# @Date:   2016-07-08 15:00:21
-# @Last Modified by:   ZwEin
-# @Last Modified time: 2016-07-08 15:00:21
