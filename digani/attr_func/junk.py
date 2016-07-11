109c577792744fc18557d54c995cc63128110020
ZwEin <zwein27@gmail.com>
2016-07-10 21:43:38 -0400

    upgrade junk

--- digani/attr_func/junk.py
+++ digani/attr_func/junk.py
@@ -2,7 +2,7 @@
 # @Author: ZwEin
 # @Date:   2016-07-07 15:54:26
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-07-09 17:22:38
+# @Last Modified time: 2016-07-10 21:42:21
 
 import re
 # from digani.common import string_helper
@@ -11,10 +11,11 @@ re_alphabet = re.compile(r'[a-zA-Z]+')
 re_digits = re.compile(r'[0-9]+')
 
 reg_junks = [
-    r'[0-9]+.*[a-z]+',
-    r'[a-z]+.*[0-9]+'
+    # r'[0-9]+.*[a-z]+',
+    # r'[a-z]+.*[0-9]+'
+    r'(?:\d+)'
 ]
-re_junks = re.compile(r'(:' + r'|'.join(reg_junks) + r')')
+re_junks = re.compile(r'^' + r'|'.join(reg_junks) + r'$')
 
 def frequency_count(attr_vals):
     freq_dict = {}
@@ -43,5 +44,6 @@ def attr_func_junk(attr_vals):
 
 
 if __name__ == '__main__':
-    text = ': 34-35 34 B Eyes: Brown Smokes: Yes but not with '
-    print re_junks.search(text)
+    # text = ': 34-35 34 B Eyes: Brown Smokes: Yes but not with '
+    text = '2015'
+    print re_junks.findall(text)
